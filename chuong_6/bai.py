f5a1204e5a401c8d7b11779dcad5cec8f1058248
Dinhngocviet2108 <ngocvietjs21@gmail.com>
2024-10-23 15:12:50 +0700

    bai1

---
+++ chuong_6/bai.py
@@ -0,0 +1,13 @@
+#Bài 8
+import math
+x = input("Nhap gia tri x: ")
+x = float(x)
+f_x = math.log(x, 4) , math.log(x, 2)
+print(f"Gia tri can tim f(x) = (f_x)")
+
+
+#Bài 1
+import math
+
+
+#Bài 2
---
+++ chuong_6/bai1.py
@@ -0,0 +1,23 @@
+import math
+
+#Nhập bán kính và chiều cao
+r = float(input("Nhập bán kính khối trụ: "))
+h = float(input("Nhập chiều cao khối trụ "))
+#Kiểm tra điều kiện
+if r > 0 and h > 0:
+    #pi
+    pi = 3.14
+    #Tính diện tích xung quanh
+    Sxq = 2 * pi * r * h
+    #Tính diện tích toàn phần
+    Stp = Sxq + 2 * pi * r ** 2
+    #Tính thể tích
+    V = pi * r ** 2 * h
+    #In kết quả,làm tròn đến số thập phân thứ hai
+    print(f"Diện tích xung quanh: {Sxq:.2f}")
+    print(f"Diện tích toàn phần: {Stp:.2f}")
+    print(f"Thể tích: {V:.2f}")
+else:
+    print("Bán kính và chiều cao phâir lớn hơn 0")
+    
+
